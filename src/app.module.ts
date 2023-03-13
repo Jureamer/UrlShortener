45c70c1a21899a16ccd6475d545bb63d882fe13f
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-03-13 16:08:15 +0900

    feat: logging, winston 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,36 @@
-import { Module, ValidationPipe } from '@nestjs/common'
-import { APP_PIPE } from '@nestjs/core'
+import { MikroOrmModule } from '@mikro-orm/nestjs'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CompModule } from './comp/comp.module'
-
+import { WinstonModule, WINSTON_MODULE_NEST_PROVIDER, utilities as nestWinstonModuleUtilities } from 'nest-winston'
+import * as winston from 'winston'
+import { LoggerMiddleware } from './lib/logger.middleware'
 @Module({
-    imports: [CompModule],
+    imports: [
+        CompModule,
+        MikroOrmModule.forRoot(),
+        WinstonModule.forRoot({
+            transports: [
+                new winston.transports.Console({
+                    level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+                    format: winston.format.combine(
+                        winston.format.colorize(),
+                        winston.format.timestamp(),
+                        nestWinstonModuleUtilities.format.nestLike('url-shortener', {
+                            prettyPrint: true,
+                            colors: true,
+                        }),
+                    ),
+                }),
+            ],
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes('*')
+    }
+}
